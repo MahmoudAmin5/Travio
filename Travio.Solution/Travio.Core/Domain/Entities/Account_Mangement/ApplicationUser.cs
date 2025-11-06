a892753363c9264e60591b92beee4af3fb1aabfc
ezzat walid <ew1139@fayoum.edu.eg>
2025-11-06 11:15:15 +0200

    Identity
    Create User and Role and Create Data base context

---
+++ Travio.Solution/Travio.Core/Domain/Entities/Account_Mangement/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace Travio.Core.Domain.Entities.Account_Mangement
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set;  }
+        public string LastName { get; set; }
+        public string? ProfilePictureURL { get; set; } // optional 
+        public DateTime RegistrationDate { get; set; }
+    }
+}
