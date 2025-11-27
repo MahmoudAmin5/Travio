e08280e8d883a424b740e71143eabdd77f3f0f1e
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2025-11-27 13:19:09 +0200

    OAuth with google

--- Travio.Solution/Travio.API/Program.cs
+++ Travio.Solution/Travio.API/Program.cs
@@ -1,9 +1,15 @@
 
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Travio.API.Middleware;
 
 using Travio.Core.Domain.Entities.Account_Mangement;
+using Travio.Core.Services.Helpers;
 using Travio.Infrastructure;
 namespace Travio.API
 {
@@ -22,7 +28,34 @@ namespace Travio.API
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); // configure the context to use SQL Server with the connection string from appsettings.json
             });
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
             builder.Services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddAuthentication(Options =>
+            {
+                Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddCookie().AddGoogle(options =>
+            {
+                var clientId = builder.Configuration["Authentication:Google:ClientId"];
+                var clientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+                options.ClientId = clientId;
+                options.ClientSecret = clientSecret;
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+                    ValidateLifetime = true,
+                };
+            });
+
             var app = builder.Build();
             app.UseMiddleware<ExceptionHandlingMiddleware>();
 
