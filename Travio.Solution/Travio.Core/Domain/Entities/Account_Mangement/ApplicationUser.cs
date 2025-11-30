847fbcc854d76b92cec8a7d6b150ef3d74fe2a73
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2025-11-30 08:02:49 +0200

    Update User table for external login providers

--- Travio.Solution/Travio.Core/Domain/Entities/Account_Mangement/ApplicationUser.cs
+++ Travio.Solution/Travio.Core/Domain/Entities/Account_Mangement/ApplicationUser.cs
@@ -7,6 +7,9 @@ namespace Travio.Core.Domain.Entities.Account_Mangement
         public string LastName { get; set; }
         public string? ProfilePictureURL { get; set; } // optional 
         public DateTime RegistrationDate { get; set; }
+        public string? LoginProvider { get; set; }
+        public string? ProviderKey { get; set; }
+
         public List<RefreshToken>? RefreshTokens { get; set; }
     }
 }
