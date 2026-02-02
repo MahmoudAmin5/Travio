42b6cbf2580512b3d923aef61eeef7a74652e586
ezzat walid <ew1139@fayoum.edu.eg>
2026-02-02 17:41:01 +0200

    add Distenation module

---
+++ Travio.Solution/Travio.Core/Domain/Entities/Destinations/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace Travio.Core.Domain.Entities.Destinations;
+
+public class Country
+{
+    public int CountryID { get; set; }
+    public int ContinentID { get; set; } // FK
+    public string Name { get; set; } = string.Empty;
+    public string FlagURL { get; set; } = string.Empty;
+
+    // Navigation
+    public Continent Continent { get; set; } = null!;
+    public ICollection<City> Cities { get; set; } = new HashSet<City>();
+}
---
+++ Travio.Solution/Travio.Core/Domain/Entities/Destinations/Interest.cs
@@ -0,0 +1,10 @@
+﻿namespace Travio.Core.Domain.Entities.Destinations;
+
+public class Interest
+{
+    public int InterestID { get; set; }
+    public string InterestName { get; set; } = null!;
+    // Navigation
+    public ICollection<DestinationInterest> DestinationInterests { get; set; } = new HashSet<DestinationInterest>();
+
+}
