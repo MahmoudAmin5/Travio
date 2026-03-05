24f46103a954fdf47ab8de1f5bcc2bddd78c1bcb
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-05 07:19:05 +0200

    Create Post.cs

---
+++ Travio.Solution/Travio.Core/Domain/Entities/Community/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travio.Core.Domain.Entities.Account_Mangement;
+
+namespace Travio.Core.Domain.Entities.Community
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public ApplicationUser User { get; set; }
+
+
+
+    }
+}
