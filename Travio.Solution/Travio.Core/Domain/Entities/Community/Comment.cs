2a5fbf89108f552154f638f018ee373a8c9494a5
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-05 07:19:09 +0200

    Create Comment.cs

---
+++ Travio.Solution/Travio.Core/Domain/Entities/Community/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travio.Core.Domain.Entities.Community
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
