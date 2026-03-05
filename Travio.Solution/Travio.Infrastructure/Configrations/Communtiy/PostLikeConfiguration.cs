b71bfd738741a706513043b33b417bccb553f711
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-05 08:15:14 +0200

    Create PostLikeConfiguration.cs

---
+++ Travio.Solution/Travio.Infrastructure/Configrations/Communtiy/PostLikeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travio.Core.Domain.Entities.Community;
+
+namespace Travio.Infrastructure.Configrations.Communtiy
+{
+    public class PostLikeConfiguration : IEntityTypeConfiguration<PostLike>
+    {
+        public void Configure(EntityTypeBuilder<PostLike> builder)
+        {
+            builder.ToTable("PostLikes", "Community");
+
+            builder.HasKey(l => new { l.PostId, l.UserId });
+
+            builder.HasOne(l => l.Post)
+                .WithMany(p => p.Likes)
+                .HasForeignKey(l => l.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(l => l.User)
+                .WithMany()
+                .HasForeignKey(l => l.UserId)
+                .OnDelete(DeleteBehavior.NoAction); 
+        }
+    }
+}
