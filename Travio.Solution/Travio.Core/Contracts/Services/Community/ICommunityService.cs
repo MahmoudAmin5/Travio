83f8df6d675bbafa3e1236f50b8f37a4af85243b
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-07 06:41:41 +0200

    Create ICommunityService.cs

---
+++ Travio.Solution/Travio.Core/Contracts/Services/Community/ICommunityService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travio.Core.Contracts.Services.Community
+{
+    public interface ICommunityService
+    {
+       
+    }
+}
