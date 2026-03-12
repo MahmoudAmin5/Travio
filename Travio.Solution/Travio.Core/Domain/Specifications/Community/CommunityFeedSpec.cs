6e9e1a795bf7d28b6d70a577ade2b636eef6342c
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-12 17:38:25 +0200

    feat: updating to project the data in the spec

--- Travio.Solution/Travio.Core/Domain/Specifications/Community/CommunityFeedSpec.cs
+++ Travio.Solution/Travio.Core/Domain/Specifications/Community/CommunityFeedSpec.cs
@@ -1,25 +1,39 @@
 ﻿using Ardalis.Specification;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Travio.Core.DTOs.CommunityDTO;
 using Travio.Core.Domain.Entities.Community;
 
-namespace Travio.Core.Domain.Specifications.Community
+public class CommunityFeedSpec
+    : Specification<Post, PostResponseDTO>
 {
-    public class CommunityFeedSpec : Specification<Post>
+    public CommunityFeedSpec(
+        string currentUserId,
+        int pageNumber,
+        int pageSize)
     {
-        public CommunityFeedSpec(int pageNumber, int pageSize)
+        Query
+            .OrderByDescending(post => post.CreatedOn)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+        .AsNoTracking();
+
+        Query.Select(post => new PostResponseDTO
         {
-            Query.Include(x => x.User)
-                .Include(x => x.Images)
-                .Include(x => x.Comments)
-                .Include(x => x.Likes)
-                .OrderByDescending(x => x.CreatedOn)
-                .Skip((pageNumber-1) * pageSize)
-                .Take(pageSize)
-                .AsNoTracking();
-        }
+            Id = post.Id,
+            Title = post.Title,
+            Content = post.Content,
+            CreationDate = post.CreatedOn,
+
+            AutherId = post.UserId,
+            AutherName = post.User.FirstName + " " + post.User.LastName,
+            AuthorProfilePictureUrl = post.User.ProfilePictureURL,
+
+            LikesCount = post.Likes.Count,
+            CommentsCount = post.Comments.Count,
+
+             PostImagesUrls = post.Images.Select(i => i.ImageUrl).ToList(),
+
+            IsLikedByCurrentUser = post.Likes
+                .Any(l => l.UserId == currentUserId)
+        });
     }
 }
