8000c261815732a777aad231dc8e7bce3ee28f16
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-14 01:17:45 +0200

    Create AddPostImageValidator.cs

---
+++ Travio.Solution/Travio.Core/Validators/AddPostImageValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travio.Core.DTOs.CommunityDTO;
+
+namespace Travio.Core.Validators
+{
+        public class AddPostImageDtoValidator : AbstractValidator<UploadPostImageDTO>
+        {
+            public AddPostImageDtoValidator()
+            {
+                RuleFor(dto => dto.image)
+                    .NotNull().WithMessage("An image file is required.")
+                    .Must(BeAValidSize).WithMessage("The image size must not exceed 5MB.")
+                    .Must(BeAValidExtension).WithMessage("Only .jpg, .jpeg, and .png files are allowed.");
+            }
+
+            private bool BeAValidSize(IFormFile file)
+            {
+                if (file == null) return false;
+
+                var maxSizeInBytes = 5 * 1024 * 1024;
+                return file.Length <= maxSizeInBytes;
+            }
+
+            private bool BeAValidExtension(IFormFile file)
+            {
+                if (file == null) return false;
+
+                var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+                var fileExtension = System.IO.Path.GetExtension(file.FileName).ToLower();
+
+                return allowedExtensions.Contains(fileExtension);
+            }
+        }
+    }
