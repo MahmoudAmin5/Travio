2852217fd14c3898beb7b276519f24ba745a58ca
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-14 01:17:25 +0200

    feat: update the column title to location in post table

---
+++ Travio.Solution/Travio.Infrastructure/Migrations/20260313211633_AddingLocationColumnToPostTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travio.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingLocationColumnToPostTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                schema: "Community",
+                table: "Posts",
+                newName: "Location");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                schema: "Community",
+                table: "Posts",
+                newName: "Title");
+        }
+    }
+}
