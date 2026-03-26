5da30c647e9cbbc92ea2779e2effc456aeebf6bc
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-26 07:49:48 +0200

    add(community) :add commint segniture

--- Travio.Solution/Travio.Core/Contracts/Services/Community/ICommunityService.cs
+++ Travio.Solution/Travio.Core/Contracts/Services/Community/ICommunityService.cs
@@ -17,5 +17,6 @@ namespace Travio.Core.Contracts.Services.Community
         Task<ServiceResponse<List<string>>> AddPostImageAsync(int postId, string UserId, List<IFormFile> image);
         Task<ServiceResponse<bool>> ToggleLikeAsync(int postId, string userId);
         Task<ServiceResponse<PostDetailsResponseDTO>> GetPostByIdAsync(int postId, string userId);
+        Task<ServiceResponse<CommentResponseDTO>> AddCommentAsync(int postId, string userId, CreateCommentDTO dto);
     }
 }
