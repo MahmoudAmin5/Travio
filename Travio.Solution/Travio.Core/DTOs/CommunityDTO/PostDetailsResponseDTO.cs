661eb3e2354d6993d4fc91445ae03565d1a9de13
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-03-26 06:52:51 +0200

    Create PostDetailsResponseDTO.cs

---
+++ Travio.Solution/Travio.Core/DTOs/CommunityDTO/PostDetailsResponseDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travio.Core.DTOs.CommunityDTO
+{
+    public class PostDetailsResponseDTO
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorProfilePictureUrl { get; set; }
+
+        public int LikesCount { get; set; }
+        public bool IsLikedByCurrentUser { get; set; }
+        public List<string> ImageUrls { get; set; }
+
+       
+        public List<CommentResponseDTO> Comments { get; set; }
+    }
+}
