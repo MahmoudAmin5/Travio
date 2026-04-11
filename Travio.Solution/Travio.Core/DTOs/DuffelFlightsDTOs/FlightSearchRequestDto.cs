9de08d14398e5624e466b80d00a6d8022baca523
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-11 05:23:41 +0200

    Create FlightSearchRequestDto.cs

---
+++ Travio.Solution/Travio.Core/DTOs/DuffelFlightsDTOs/FlightSearchRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travio.Core.DTOs.DuffelFlightsDTOs
+{
+    public class FlightSearchRequestDto
+    {
+        public string Origin { get; set; } 
+        public string Destination { get; set; } 
+        public string DepartureDate { get; set; }
+        public int NumberOfAdults { get; set; }
+    }
+}
