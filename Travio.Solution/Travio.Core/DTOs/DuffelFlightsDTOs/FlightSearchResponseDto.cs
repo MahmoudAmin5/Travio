ecd8e45f64535da46e66e1182a8ae7d6519c24c5
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-24 15:44:47 +0300

    Refactor(Flights) : matching the new response

--- Travio.Solution/Travio.Core/DTOs/DuffelFlightsDTOs/FlightSearchResponseDto.cs
+++ Travio.Solution/Travio.Core/DTOs/DuffelFlightsDTOs/FlightSearchResponseDto.cs
@@ -8,13 +8,12 @@ namespace Travio.Core.DTOs.DuffelFlightsDTOs
 {
     public class FlightSearchResponseDto
     {
-        public string OfferId { get; set; } 
-        public string AirlineName { get; set; }
-        public string Origin { get; set; }
-        public string Destination { get; set; }
-        public DateTime DepartureTime { get; set; }
-        public DateTime ArrivalTime { get; set; }
+        public string OfferId { get; set; }
+        public string TotalOrigin { get; set; }
+        public string TotalDestination { get; set; }
         public decimal TotalPrice { get; set; }
         public string Currency { get; set; }
+        public int Stops { get; set; }
+        public List<FlightSegmentDto> Segments { get; set; }
     }
 }
