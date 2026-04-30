22817b42cd2db51e25a764c3e95c6d3fc550eae0
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-04-30 04:53:21 +0300

    add(Flight) : add FlightDetails Dtos

---
+++ Travio.Solution/Travio.Core/DTOs/DuffelFlightsDTOs/FlightSegmentDetailsDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travio.Core.DTOs.DuffelFlightsDTOs
+{
+    public class FlightSegmentDetailsDto
+    {
+        public string AirlineName { get; set; }
+        public string AirlineLogoUrl { get; set; }
+        public string FlightNumber { get; set; }
+
+       
+        public string AircraftName { get; set; }
+
+        public string OriginAirport { get; set; }
+        public string DepartureTime { get; set; }
+
+        public string DestinationAirport { get; set; }
+        public string ArrivalTime { get; set; }
+
+        public string SegmentDuration { get; set; }
+    }
+}
