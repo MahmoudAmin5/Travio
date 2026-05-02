2f26280064bc35d1f7fffec041b3ee63349da6e0
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-05-02 23:33:37 +0300

    add(Flight) : create order service

--- Travio.Solution/Travio.Core/Contracts/Services/DuffelFlights/IFlightBookingService.cs
+++ Travio.Solution/Travio.Core/Contracts/Services/DuffelFlights/IFlightBookingService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Travio.Core.DTOs.DuffelFlightsDTOs;
+using Travio.Core.DTOs.DuffelFlightsDTOs.Requests;
 using Travio.Core.DTOs.GenericResponse;
 
 namespace Travio.Core.Contracts.Services.DuffelFlights
@@ -13,5 +14,6 @@ namespace Travio.Core.Contracts.Services.DuffelFlights
         Task<ServiceResponse<List<FlightSearchResponseDto>>> SearchFlightsAsync(FlightSearchRequestDto request);
         Task<ServiceResponse<List<TopFlightOfferDto>>> GetTopOffersAsync();
         Task<ServiceResponse<FlightDetailsDto>> GetFlightDetailsAsync(string offerId);
+        Task<ServiceResponse<FlightOrderResponseDto>> CreateOrderAsync(FlightOrderRequestDto request);
     }
 }
---
+++ Travio.Solution/Travio.Core/DTOs/DuffelFlightsDTOs/Requests/FlightOrderRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travio.Core.DTOs.DuffelFlightsDTOs.Requests
+{
+    public class FlightOrderRequestDto
+    {
+        public string OfferId { get; set; }
+        public List<PassengerDetailsDto> Passengers { get; set; } = new();
+    }
+    public class PassengerDetailsDto
+    {
+        public string Title { get; set; } 
+        public string GivenName { get; set; } 
+        public string FamilyName { get; set; }
+        public DateOnly BornOn { get; set; } 
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; } 
+        public string Gender { get; set; } 
+    }
+}
