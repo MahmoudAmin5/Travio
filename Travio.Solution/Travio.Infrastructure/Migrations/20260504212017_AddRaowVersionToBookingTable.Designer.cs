ec1fe7e5ed25d65ecbee3b604bb05d74211f8191
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-05-05 07:23:36 +0300

    update fight booking table

--- Travio.Solution/Travio.Infrastructure/Migrations/20260504212017_AddRaowVersionToBookingTable.Designer.cs
+++ Travio.Solution/Travio.Infrastructure/Migrations/20260504212017_AddRaowVersionToBookingTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Travio.Infrastructure;
 
@@ -11,9 +12,11 @@ using Travio.Infrastructure;
 namespace Travio.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260504212017_AddRaowVersionToBookingTable")]
+    partial class AddRaowVersionToBookingTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -688,10 +691,6 @@ namespace Travio.Infrastructure.Migrations
                     b.Property<string>("PNR")
                         .IsRequired()
                         .HasColumnType("nvarchar(450)");
-
-                    b.Property<string>("PassengersJson")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<byte[]>("RowVersion")
                         .IsConcurrencyToken()
