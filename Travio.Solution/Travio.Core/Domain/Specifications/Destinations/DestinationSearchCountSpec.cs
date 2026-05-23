5b4d576d04c51b3ed89ba191443b819deddd12bc
ezzat walid <ew1139@fayoum.edu.eg>
2026-05-23 19:11:05 +0300

    Improve destination search to include City, Country, Continent, and Interest

--- Travio.Solution/Travio.Core/Domain/Specifications/Destinations/DestinationSearchCountSpec.cs
+++ Travio.Solution/Travio.Core/Domain/Specifications/Destinations/DestinationSearchCountSpec.cs
@@ -7,6 +7,16 @@ public class DestinationSearchCountSpec : Specification<Destination>
 {
     public DestinationSearchCountSpec(string keyword)
     {
-        Query.Where(x => x.Name.Contains(keyword) || x.Description.Contains(keyword));
+        var lowerKeyword = keyword.ToLower();
+
+        Query.Where(x =>
+            x.Name.ToLower().Contains(lowerKeyword) ||
+            x.Description.ToLower().Contains(lowerKeyword) ||
+            x.City.Name.ToLower().Contains(lowerKeyword) ||
+            x.City.Country.Name.ToLower().Contains(lowerKeyword) ||
+            x.City.Country.Continent.Name.ToLower().Contains(lowerKeyword) ||
+            x.DestinationInterests.Any(di =>
+                di.Interest.InterestName.ToLower().Contains(lowerKeyword))
+        );
     }
-}
+}
--- Travio.Solution/Travio.Core/Domain/Specifications/Destinations/DestinationSearchSpec.cs
+++ Travio.Solution/Travio.Core/Domain/Specifications/Destinations/DestinationSearchSpec.cs
@@ -7,14 +7,25 @@ public class DestinationSearchSpec : Specification<Destination>
 {
     public DestinationSearchSpec(string keyword, int skip, int take)
     {
-        Query.Where(x => x.Name.Contains(keyword) || x.Description.Contains(keyword));
+        var lowerKeyword = keyword.ToLower();
+
+        Query.Where(x =>
+            x.Name.ToLower().Contains(lowerKeyword) ||
+            x.Description.ToLower().Contains(lowerKeyword) ||
+            x.City.Name.ToLower().Contains(lowerKeyword) ||
+            x.City.Country.Name.ToLower().Contains(lowerKeyword) ||
+            x.City.Country.Continent.Name.ToLower().Contains(lowerKeyword) ||
+            x.DestinationInterests.Any(di =>
+                di.Interest.InterestName.ToLower().Contains(lowerKeyword))
+        );
 
         Query.Include(x => x.City)
+                .ThenInclude(c => c.Country)
              .Include(x => x.Images)
              .Include(x => x.DestinationInterests)
-                .ThenInclude(di => di.Interest)
-                    .AsNoTracking();
+                .ThenInclude(di => di.Interest);
 
+        Query.AsNoTracking();
         Query.OrderByDescending(x => x.Rating);
         Query.Skip(skip).Take(take);
     }
